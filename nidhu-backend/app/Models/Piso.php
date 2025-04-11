0bd13f8fd149568a656f0215dd2bd8ffb8cb0ce7
Sunderfal <sergiovdg2016@gmail.com>
2025-04-11 12:56:31 +0200

    Implementado relaciones en los modelos Usuario y Piso e instalado Sanctum

--- nidhu-backend/app/Models/Piso.php
+++ nidhu-backend/app/Models/Piso.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Piso extends Model {
     
@@ -13,5 +14,32 @@ class Piso extends Model {
     protected $primaryKey = "idPiso";
 
     public $timestamps = false;
- 
+    
+    /**
+     * Devuelve la relación de los usuarios que tiene el piso
+     *
+     * @return HasMany
+     */
+    public function usuarios(): HasMany {
+        return $this->hasMany(Usuario::class, "idPiso");
+    }
+
+    /**
+     * Devuelve el usuario propietario del piso
+     *
+     * @return Usuario
+     */
+    public function propietario(): Usuario {
+        return $this->hasOne(Usuario::class, "idPiso")->getResults();
+    }
+
+    /**
+     * Devuelve la relación de los productos que tiene que comprar un piso
+     *
+     * @return HasMany
+     */
+    public function productos(): HasMany {
+        return $this->hasMany(Producto::class, "idPiso");
+    }
+
 }
--- nidhu-backend/app/Models/Usuario.php
+++ nidhu-backend/app/Models/Usuario.php
@@ -4,16 +4,64 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Usuario extends Authenticatable {
 
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = "usuario";
     protected $primaryKey = "idUsuario";
     protected $fillable = ["nombre", "email", "password", "avatar", "idPiso"];
     protected $hidden = ["password"];
+
+    /**
+     * Devuelve el piso al que pertenece el usuario
+     *
+     * @return Piso
+     */
+    public function piso(): Piso {
+        return $this->belongsTo(Piso::class, "idPiso")->getResults();
+    }
+
+    /**
+     * Devuelve la relacion de las tareas asignadas al usuario
+     *
+     * @return HasMany
+     */
+    public function tareas(): HasMany {
+        return $this->hasMany(Tarea::class, "idUsuario");
+    }
+
+    /**
+     * Devuelve la relación de los gastos que tiene el usuario
+     *
+     * @return HasMany
+     */
+    public function gastos(): HasMany {
+        return $this->hasMany(Gasto::class, "idUsuario");
+    }
+
+    /**
+     * Devuelve la relación de las invitaciones que el usuario ha hecho a otros usuarios
+     *
+     * @return BelongsToMany
+     */
+    public function usuariosInvitados(): BelongsToMany {
+        return $this->belongsToMany(Usuario::class, "invitaciones_piso", "idUsuario", "idInvitado");
+    }
+
+    /**
+     * Devuelve la relación de las invitaciones de otros usuarios que tiene el usuario
+     *
+     * @return BelongsToMany
+     */
+    public function invitaciones(): BelongsToMany {
+        return $this->belongsToMany(Usuario::class, "invitaciones_piso", "idInvitado", "idUsuario");
+    }
 
 }
